f48727b75e19ecfbd8fffcbbd184bf24f65fb07d
MercyM <MercyM@S-MERCYM-Z15UG6>
2023-12-05 21:19:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Static attribute shows all that is share by
+            //its an attribute on the class itself not on the
+            Songs holiday = new Songs("Holiday", "Green Day", 5);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin",10);
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Songs.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace _22.Static_Attribute
+{
+    internal class Songs
+    {
+        public string title;
+        public string artist;
+        public int duration;
+
+        public static int songCount = 0;//declaration of the
+        public Songs(string aTitle, string aArtist, int aDuration)
+        {
+            this.title = aTitle;
+            this.artist = aArtist;
+            this.duration = aDuration;
+            songCount++; //increment each time song is added
+        }
+     
+    }
+
+}
